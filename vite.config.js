850e156cb983e90772e7987c13033564cb5c886e
Kevin <kevin.calva@us.isobar.com>
2023-10-20 00:14:26 -0500

    needed to add vite config edit in order for build to work

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Set the base path to the root if your app is deployed in a subdirectory
   plugins: [react()],
 })
