600f26a17941ba6edf4562c869e4ba4dd2fe53ce
Kevin <kevin.calva@us.isobar.com>
2023-10-23 23:37:16 -0500

    getting rid script file

--- src/music.sh
+++
@@ -1,3 +0,0 @@
-YOUTUBE_MUSIC_LINK="https://music.youtube.com/watch?v=zuaBRqUBhyw&list=RDAMVM-SttFgyRNCI"
-
-cd /Users/kev/Documents/Music && youtube-dl --audio-quality 0 -i --extract-audio --audio-format mp3 -o './%(title)s.%(ext)s' --add-metadata --embed-thumbnail --metadata-from-title "%(artist)s - %(title)s" ${YOUTUBE_MUSIC_LINK}
