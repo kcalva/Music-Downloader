04122bd7909c91ba49b0c8a4224a271c08049724
Kevin <kevin.calva@us.isobar.com>
2023-10-30 23:18:37 -0500

    refactored my code and added user input for the backend input

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,56 @@ import { useState } from 'react'
 import './App.css'
 
 const App = () => {
-  const [youtubeLink, setYoutubeLink] = useState("")
-  const buttonLabel = "Downloading file"
-  const runScript = () => {
-    console.log(youtubeLink)
-    fetch('http://localhost:5000/execute-script', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ youtubeLink }),
-    })
-      .then(response => response.text())
-      .then(data => {
-        console.log('Script execution response:', data)
-        // Handle the response data as needed
+  const [youtubeLink, setYoutubeLink] = useState('')
+  const [userName, setUserName] = useState('')
+  const buttonLabel = 'Download Audio'
+
+  const runScript = async () => {
+    const requestData = { youtubeLink, user: userName }
+
+    try {
+      const response = await fetch('http://localhost:5000/download-audio', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
       })
-      .catch(error => {
-        console.error('Error executing script:', error)
-        // Handle the error condition
-      })
+
+      if (!response.ok) {
+        throw new Error('Failed to download audio')
+      }
+
+      await response.json()
+      // Handle the response data as needed
+    } catch (error) {
+      console.error('Error executing script:', error)
+      // Handle the error condition
+    }
   }
+
   return (
     <div className='container'>
       <h1>Youtube Music Downloader</h1>
-      <p className="read-the-docs">
-        copy the youtube music link here
-      </p>
+      <p className='read-the-docs'>Copy the YouTube Music link here</p>
       <input
-        className="input-label"
-        placeholder={"Insert Youtube Music Url"}
-        type="text"
+        className='input-label'
+        placeholder='Insert YouTube Music Url'
+        type='text'
         name='value'
         value={youtubeLink}
-        onChange={(e) => {
-          setYoutubeLink(e.target.value)
-        }}
+        onChange={(e) => setYoutubeLink(e.target.value)}
       />
-      <div
-        className="standard-button"
-        onClick={runScript}>
-        <div className="label">{buttonLabel}</div>
+      <input
+        className='input-label'
+        placeholder='Your Name'
+        type='text'
+        name='userName'
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <div className='standard-button' onClick={runScript}>
+        <div className='label'>{buttonLabel}</div>
       </div>
     </div>
   )
